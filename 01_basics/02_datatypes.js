15aa6ba1ffdbe0ddcfe2c321dc79fffdf7e5aa0a
Shivam6050 <160846295+Shivam6050@users.noreply.github.com>
2025-04-10 11:07:24 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3)//we are using nodejs not browser 
+
+let name = "shivam";
+let age = 20;
+let isLoggedIn = false;
+
+//number =>2 to the power 53
+//bigint for larger number
+//string =>""
+//boolean=> true/false
+//null=>standalone value(it's not undefined it's empty).It is an object.
+//undefined => when value is not declared
+//symbol => unique
+
+
+console.log(typeof"shivam");//string
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
