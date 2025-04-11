1fae7dd858dec06cc18460a17a194e729bb46e37
Shivam6050 <160846295+Shivam6050@users.noreply.github.com>
2025-04-11 11:08:41 +0000

    container commit done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,9 +21,50 @@ console.log(booleanIsLoggedIn);
 // "hitesh" => true
 
 let someNumber = 33;
-let stringNumber = string(someNumber);
+let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+ // ******************************Operations*****************************************
+
+ let value = 3;
+ let negValue = -value;
+ console.log(negValue);/*it will return the negative of value */
+
+ //These are arithmetic operation 
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**2);
+ console.log(2/2);
+ console.log(2%2);
+
+ let str1 = "hello";
+ let str2 = "shivam";
+ let str3 =str1+str2;
+ console.log(str3);/*This will return hello shivam*/
+
+console.log("1"+2);/*This will print 12 */
+console.log(1+"2");/*This will print 12 */
+console.log("1"+2+2);/*This will print 122 */
+console.log(1+2+"2");/*This will print 32 */
+
+console.log((3+4)*5%3);
+
+console.log(true);/*This will return true */
+
+//These are bad practices as these are tricky conversion and not good for code readability
+console.log(+true);/*This will return 1*/
+console.log(+"");/*This will return 0 as empty string is false and the conversion will return 0 */
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);/*This will return 101 and this is Postfix incrementation */
+
+let gameCounter1 = 100;
+++gameCounter1;
+console.log(gameCounter1);/*This will return 101 and this is Prefix incrementation */
+
+ 
  
