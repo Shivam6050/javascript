73d5bf517bf635e74355bc51ad4fba5c6e870c62
Shivam6050 <160846295+Shivam6050@users.noreply.github.com>
2025-06-18 06:22:21 +0000

    objects basics

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,4 +28,34 @@ const obj2 = {3:"c",4:"d"};
 
 //const obj3 = {obj1,obj2};
 const obj3 = Object.assign({},obj1,obj2);  //curly braces is used as target and if we don't use this then first object become target and next object become source
+const obj4 = {...obj1,...obj2};
 console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com",
+    },
+    {
+        id: 1,
+        email: "h@gmail.com",
+    },
+    {
+        id: 1,
+        email: "h@gmail.com",
+    },
+    {
+        id: 1,
+        email: "h@gmail.com",
+    }
+]
+
+users[1].email;
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
